c5c742a409c7449fac0d5921986d7d714af6e167
Simon Lomax <simax.99.99@gmail.com>
2016-12-19 12:51:40 +0000

    Added config to suave statWebServer

--- src/PowershopAzureImages/Program.fs
+++ src/PowershopAzureImages/Program.fs
@@ -3,9 +3,16 @@
 open Suave
 open Suave.Filters
 open Suave.Operators
+open System.Net
+open System
 
 [<EntryPoint>]
-let main argv =
+let main [| port |] =
 
-    startWebServer defaultConfig (Successful.OK "Hello World!")
+    let config =
+            { defaultConfig with
+                bindings = [ HttpBinding.mk HTTP IPAddress.Loopback (uint16 port) ]
+                listenTimeout = TimeSpan.FromMilliseconds 3000. }
+
+    startWebServer config (Successful.OK "Hello World!")
     0
