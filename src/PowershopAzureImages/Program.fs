09f67f6a5cc27ea8ab87f0858e50acf6a723cb59
simax <simax.99.99@gmail.com>
2016-12-19 14:36:24 +0000

    Amended startWebServer config a la https://blog.vbfox.net/2016/03/31/Suave-in-IIS-hello-world.html

--- src/PowershopAzureImages/Program.fs
+++ src/PowershopAzureImages/Program.fs
@@ -1,18 +1,33 @@
-﻿module PowershopAzureImages
+﻿module IISHelpers =
+    open System
 
-open Suave
-open Suave.Filters
-open Suave.Operators
-open System.Net
-open System
+    /// Port specified by IIS HttpPlatformHandler
+    let httpPlatformPort =
+        match Environment.GetEnvironmentVariable("HTTP_PLATFORM_PORT") with
+        | null -> None
+        | value ->
+            match Int32.TryParse(value) with
+            | true, value -> Some value
+            | false, _ -> None
 
-[<EntryPoint>]
-let main argv =
 
-    // let config =
-    //         { defaultConfig with
-    //             bindings = [ HttpBinding.mk HTTP IPAddress.Loopback (uint16 port) ]
-    //             listenTimeout = TimeSpan.FromMilliseconds 3000. }
+module PowershopAzureImages =
 
-    startWebServer defaultConfig (Successful.OK "Hello World!")
-    0
+    open Suave
+    open Suave.Filters
+    open Suave.Operators
+    open System.Net
+    open System
+
+    [<EntryPoint>]
+    let main argv =
+        let config = defaultConfig
+        let config =
+            match IISHelpers.httpPlatformPort with
+            | Some port ->
+                { config with
+                    bindings = [ HttpBinding.mkSimple HTTP "127.0.0.1" port ] }
+            | None -> config
+
+        startWebServer config (Successful.OK "Hello World!")
+        0
