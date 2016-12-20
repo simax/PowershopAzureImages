b580a6d9eb63ec4c6d76f8b06cf81a8f0eb342f8
Simon Lomax <simax.99.99@gmail.com>
2016-12-20 17:21:23 +0000

    Added Suave.Swagger. Now trying to figure out how to upload files

--- src/PowershopAzureImages/Program.fs
+++ src/PowershopAzureImages/Program.fs
@@ -14,10 +14,59 @@
 module PowershopAzureImages =
 
     open Suave
+    open Suave.Successful
+    open Suave.RequestErrors
     open Suave.Filters
     open Suave.Operators
     open System.Net
     open System
+
+    open Suave.Swagger
+    open Rest
+    open FunnyDsl
+    open Swagger
+
+    let now : WebPart =
+        fun (x : HttpContext) ->
+            async {
+            // The MODEL helper checks the "Accept" header 
+            // and switches between XML and JSON format
+            return! MODEL DateTime.Now x
+            }
+
+    [<CLIMutable>] 
+    type Pet =
+        { 
+          Id:int
+          Name:string
+          Category:PetCategory }
+    and [<CLIMutable>] PetCategory = 
+        { Id:int
+          Name:string }
+
+    let uploadImage =
+        let upload r = 
+            //let s = match r[0] with 
+            //          | (s, None) -> None
+            //          | (_ ,Some x) -> x
+                
+            match r.files with
+            | [] -> "No filename supplied !!!!!!!" |> BAD_REQUEST 
+            | x::xs -> x.fileName |> OK 
+        request upload 
+
+
+    let imageApi = 
+        swagger {
+            // syntax 1
+            for route in getting (simpleUrl "/time" |> thenReturns now) do
+                yield description Of route is "What time is it ?"
+
+            for route in posting <| simpleUrl "/pictures" |> thenReturns uploadImage do
+                yield description Of route is "Post an image"
+                yield route |> addResponse 200 "Returns the URL of the created image" None
+                yield parameter "File to upload" postOf route (fun p -> { p with Name = "Files"; Type = (Some typeof<System.IO.File>); In=FormData })
+        }
 
     [<EntryPoint>]
     let main argv =
@@ -29,11 +78,15 @@ module PowershopAzureImages =
                     bindings = [ HttpBinding.mkSimple HTTP "127.0.0.1" port ] }
             | None -> config
 
+        
+
         let routes = 
             choose [
                 GET >=> path "/" >=> (Successful.OK "Welcome to Powershop AZURE Images API")
+                POST >=> path "/pictures" >=> uploadImage
+                
                 RequestErrors.NOT_FOUND "Found no handlers"
         ]        
 
-        startWebServer config routes
+        startWebServer config imageApi.App
         0
