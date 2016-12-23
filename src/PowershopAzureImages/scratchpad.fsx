7d2f04d2b52d99f281fb7201814262fabaa24bd5
Simon Lomax <simax.99.99@gmail.com>
2016-12-23 16:45:10 +0000

    The scratchpad script works in Vis Studio and Xamarin and Fsharpi on mac - not in Ionide

--- src/PowershopAzureImages/scratchpad.fsx
+++ src/PowershopAzureImages/scratchpad.fsx
@@ -1,19 +1,22 @@
+
 #r "../../packages/WindowsAzure.Storage/lib/net45/Microsoft.WindowsAzure.Storage.dll"
-#r "../../packages/Microsoft.WindowsAzure.ConfigurationManager/lib/net40/Microsoft.WindowsAzure.Configuration.dll"
+//#r "../../packages/Microsoft.WindowsAzure.ConfigurationManager/lib/net40/Microsoft.WindowsAzure.Configuration.dll"
+
+//#r "packages/WindowsAzure.Storage.8.0.0/lib/net45/Microsoft.WindowsAzure.Storage.dll"
+//#r "packages/Microsoft.WindowsAzure.ConfigurationManager.3.2.3/lib/net40/Microsoft.WindowsAzure.Configuration.dll"
 
 open System
 open System.IO
-open Microsoft.Azure
+//open Microsoft.Azure
 open Microsoft.WindowsAzure // Namespace for CloudConfigurationManager
 open Microsoft.WindowsAzure.Storage // Namespace for CloudStorageAccount
 open Microsoft.WindowsAzure.Storage.Blob // Namespace for Blob storage types
 
-open Microsoft.WindowsAzure.Storage
 
 // Parse the connection string and return a reference to the storage account.
-// Parse the connection string and return a reference to the storage account.
-let storageConnString = 
-    CloudConfigurationManager.GetSetting("StorageConnectionString")
+let storageConnString = "DefaultEndpointsProtocol=https;AccountName=powershop;AccountKey=3yOHdp1t0dGNi91+YAsIYd3Q6D9ZGQCd/xqpq0SJ4TJLSS2TnnPaFMBYgMr/KKmLwm5kE9rLVnA8H/jrVxf9Fg==";
+
+//CloudConfigurationManager.GetSetting("StorageConnectionString")
 
 //let storageConnString = @"DefaultEndpointsProtocol=https;AccountName=powershop;AccountKey=3yOHdp1t0dGNi91+YAsIYd3Q6D9ZGQCd/xqpq0SJ4TJLSS2TnnPaFMBYgMr/KKmLwm5kE9rLVnA8H/jrVxf9Fg==" 
 
@@ -38,9 +41,9 @@ let blockBlob = container.GetBlockBlobReference("myblob.txt")
 
 // Create or overwrite the "myblob.txt" blob with contents from the local file.
 // Create a dummy file to upload
-// let localFile = __SOURCE_DIRECTORY__ + "/myfile.txt"
-// File.WriteAllText(localFile, "some data")
-// blockBlob.UploadFromFile(localFile) 
+//let localFile = __SOURCE_DIRECTORY__ + "/myfile.txt"
+//File.WriteAllText(localFile, "some data")
+//blockBlob.UploadFromFile(localFile) 
 
 // Loop over items within the container and output the length and URI.
 for item in container.ListBlobs(null, false) do
@@ -56,6 +59,3 @@ for item in container.ListBlobs(null, false) do
 
     | _ ->
         printfn "Unknown blob type: %O" (item.GetType())
-
-
-
